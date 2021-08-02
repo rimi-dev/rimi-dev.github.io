bb630eb0bb946a6be69734fac91dd411faf4ef59
rimi <yerimi@kakao.com>
2021-08-02 17:14:39 +0900

    [CHG] flutter init

---
+++ rimi/lib/sections/home/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(children: [
+          Text('data')
+        ],),
+      ),
+    );
+  }
+}
